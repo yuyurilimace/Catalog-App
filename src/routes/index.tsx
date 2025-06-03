3912f5b8b0221052179a8c27cf6f591956bd0a80
José Yuri Lima Lira <yurilima.ce@gmail.com>
2025-06-03 14:27:57 -0300

    refactor: refactoring project

--- src/routes/index.tsx
+++
@@ -1,4 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { MountRoutes } from "./mountRoutes";
-
-export const router = createBrowserRouter(MountRoutes());
--- src/theme/index.ts
+++
@@ -1,5 +0,0 @@
-import { Colors } from "./colors";
-
-export const theme = {
-  colors: Colors,
-};
